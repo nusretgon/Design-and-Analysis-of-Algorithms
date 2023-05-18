393fae8060cd6587660a0c638b23f3570c9be7b9
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2023-05-18 13:53:04 +0300

    Add files via upload

---
+++ Fibonacci/BruteForcefibonacci.java
@@ -0,0 +1,21 @@
+package Fibonacci;
+public class BruteForcefibonacci {
+	// It's brute force 
+	public static int No_memoizationFibo(int n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return No_memoizationFibo(n-1)+No_memoizationFibo(n-2);  
+		// problem size=n
+		// time complexity = T(0)=T(1)=1 T(n-1)+T(n-2)=Theta (2^n)
+		// space complexity = 0 (constant)
+	}
+	
+	public static void main(String[] args) {
+		int x=No_memoizationFibo(6);
+		System.out.println(x);
+		
+		int y=No_memoizationFibo(20);
+		System.out.println(y);
+		
+	}
+}
