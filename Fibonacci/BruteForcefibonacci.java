87b5ef33310b9b402a8088a21ce234ea464d5843
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2023-05-18 13:58:27 +0300

    Update BruteForcefibonacci.java
    if statement changed.

--- Fibonacci/BruteForcefibonacci.java
+++ Fibonacci/BruteForcefibonacci.java
@@ -2,8 +2,7 @@ package Fibonacci;
 public class BruteForcefibonacci {
 	// It's brute force 
 	public static int No_memoizationFibo(int n) {
-		if(n==0) return 0;
-		if(n==1) return 1;
+		if(n==0 || n==1) return n;
 		return No_memoizationFibo(n-1)+No_memoizationFibo(n-2);  
 		// problem size=n
 		// time complexity = T(0)=T(1)=1 T(n-1)+T(n-2)=Theta (2^n)
